98e937671a5e109226b004ddb8dec0f2676333bb
Jay <blaqsparrow@gmail.com>
2017-09-17 01:38:59 -0700

    Added conditionals and loops
    An interactive, compile-able tutorial on starting C++ featuring examples using the math library, basic input/output with the console/terminal, conditional statements with if and else, and the three kinds of loops.

--- Tutorial-TheVeryBasics.cpp
+++ Tutorial-TheVeryBasics.cpp
@@ -33,12 +33,11 @@
 		Input and Output from the Console
 		- output text and numbers with "cout" 
 		- input numbers with "cin"
-		
-		TO BE ADDED LATER:		
+			
 		Program Control (jumping around)
-		X if, else if, else (making decisions)
-		X while loop (a simple loop which goes until a certain condition is met)
-		X the "for" loop (A little more complicated, but just a loop that goes a set number of times)
+		- if, else if, else (making decisions)
+		- while and do-while loops (simple loops which go until a certain condition is met)
+		- the "for" loop (A little more complicated, but just a loop that goes a set number of times)
 	
 */
 
@@ -64,6 +63,9 @@
 // Actually there are other very complicated ways of doing those things, 
 // but they're awful, so lets use these.
 #include <cmath> 
+
+//And here we need string for the objects called strings which can hold lines of text
+#include <string>
 
 //There are lots of other "includes" that come with the compiler
 // (which is the thing that turns your program from text into a program that runs).
@@ -219,6 +221,8 @@ int main()	// don't forget the parenthesis! They show that main is a function.
 	//Q: How do we know any of what you're telling us is true?
 	//A: Let's output some of our values, and move away from math for now.
 	
+	// SECTION 3: OUTPUT TO CONSOLE WINDOW AND INPUT FROM USER
+	
 	//First, let's check our circleArea calculation.
 	//According to Windows Calculator, I should get 5,541.769441656
 	cout << "Circle Area is: " << circleArea <<endl;
@@ -264,11 +268,11 @@ int main()	// don't forget the parenthesis! They show that main is a function.
 		
 	*/
 	
-	//INPUT
+	
 	// Often times the point of a program is to accept user input and process it.
 	// Let's make our area of a circle equation slightly interactive.
 	
-	// 4 STEPS:
+	// 4 STEPS TO GETTING INPUT:
 	// 1. Output a prompt to the user to tell them what you want
 	cout << "Enter a number for the radius of a circle in meters:"; //Do science in metric.
 	float userRadius; //Here we make a variable to hold what we get from the user through cin
@@ -300,24 +304,206 @@ int main()	// don't forget the parenthesis! They show that main is a function.
 	cout << "A circle with a radius of "<<userRadius<<" has an area of "<<userArea<<endl;
 	
 	
-	//Sorry kids, ran out of time to work on this. Hopefully this will help with the first
-	//few assignments before I can update it with the other basic essentials. TTYL
-	
-	//PROGRAM CONTROL - Conditionals
+	//SECTION 4: PROGRAM CONTROL - Conditionals
 	// IF by itself
+	/*
+		Now, here's where our computer really becomes more than a calculator:
+		conditional execution. This is one of the main reasons you create flow charts.
+		The simplest methods of decision making are based on true and false.
+	*/
 	
+	bool isDoorOpen = false; //here is a "bool" variable. They can hold either "true" or "false."
+	
+	//Here is our test. The important keyword here is "if"
+	// after the "if" is our test within a pair of parenthesis.
+	// VERY IMPORTANT NOTE: testing equality is done with ==, not with =.
+	// A single equals sign sets the variable on the left to the value on the right,
+	// which then becomes either "true" or "false" based on what you assigned.
+	// DO NOT DO ASSIGNMENTS WITH "=" IN A TEST. While it will compile, it is never recommended.
+	if ( isDoorOpen == false ) 
+	{ //notice the curly braces, these make up the code block that happens ONLY IF the condition is true
+		//here in the code block, since "isDoorOpen == false" is true, since we set it to false ourselves,
+		//we will execute the code to open the door.
+		cout<<"Opening door.."<<endl;
+		isDoorOpen = true; //now our bool type variable is set to true
+	}
+	
+	//The other type of conditional test involves inequalities, meaning less than or greater than
+	// Let's set some variables for a simple business scenario where a conditional might be useful.
+	int chickens = 27; //Note we use an integer type here since we don't want fractions of chickens.
+	//But since these following values represent dollars,
+	// we'll use floating types to hold the dollars and cents
+	float chickenPrice = 1.50, farmFunds = 200.75; 
+	
+	if ( chickens < 30 ){ //if value held in variable "chickens" is less than 30,
+		//do this stuff...
+		cout << "Buying more chickens!"<<endl; //Inform user that chickens are being bought.
+		farmFunds = farmFunds - (chickenPrice * 5 );  // Subtract price of 5 chickens from farmFunds
+		chickens = chickens + 5; // Then set chickens to 5 more than its current amount.
+		cout << "Current Chicken Population: "<<chickens<<endl; //output any numbers that changed
+		cout << "Funds remaining: $"<< farmFunds <<endl; //so user knows what happened
+	} //end block of code that executes when chickens < 30
+	
+		
 	// IF with ELSE
+	//Sometimes decisions have two parts to them, meaning if a certain condition is set,
+	//one action is to be taken, but otherwise something else is to be done. 
+	//That's where if and else comes in.
+	
+	short tinyNum = 4;
+	short userGuess;
+	
+	cout << "Guess a number between 1 and 10: "; //notice no "endl" here since we want their
+	// input on the same line, mostly because that just looks better after short prompts.
+	cin >> userGuess; //now we use "cin" to get a number from the user and put it in variable userGuess
+	
+	//Now we use conditionals to determine if userGuess and tinyNum hold the same value
+	if ( userGuess == tinyNum ){ //notice the "==" to test equality
+		cout <<"Congrats, you either guessed correctly or cheated!"<<endl;
+	}
+	else{ //notice the "else" block doesn't have a test, it just happens if the block above doesn't happen
+	
+		cout <<"Wow, you didn't get the answer right? Did you even read the source code?"<<endl;
+	}
+	
 	
 	// IF, ELSE IF, ELSE
+	// often times it is not enough to have simply "if this" and "if not this"
+	// and we need a slightly more complicated form of decision making. That's where we get...
+	
+	cout<<"Okay, I'll give you one more guess. Pick a number between 1 and 10: ";
+	cin >> userGuess; //get another number from user, put it in userGuess
+	
+	if ( userGuess < tinyNum ){
+		cout<<"Too bad! Your guess was too low."<<endl;
+	}
+	// Here we use else if to test another condition *only after the above test fails*
+	else if ( userGuess > tinyNum ){ 
+		cout<<"So sad! Your guess was too high."<<endl;
+	}
+	else{ // if guess wasn't < or >, then it was == (meaning equal)
+		//This final else block only is stepped into if all other tests return "false" 
+		cout<<"Your guess was exactly right! Hacker."<<endl;
+	}
+	
+	//Special Note: Remember the above collection of tests; these will be crucial for "binary 
+	// search" algorithms which efficiently jump through a sorted list to find a specific value.
+	// You will cover that after "arrays" of data, and how to sort and search them in various ways.
+	
 	
 	//PROGRAM CONTROL - Loops
 	//While
+	/*
+		Loops can be intimidating at first, but they work on the same basic idea as
+		the "if" block. If the given test is true, the code block will be entered,
+		and if not, then the block will be skipped. The difference between the if
+		and the while blocks then comes down to how many times the blocks are
+		executed. The if block by itself is either executed once or zero times.
+		The while block continues to execute until either you shut down the program or
+		computer, or you change a variable involved in the test so that the test
+		returns a "false" result.
+	*/
+	
+	//Let's simulate what Michio Kaku calls the lowest level of consciousness: the thermostat
+	float roomTempFaren = 51.2; //let's assume our room temperature starts here
+	while ( roomTempFaren < 72 ){
+		cout<<"Room temp too low at "<<roomTempFaren;
+		cout<<". Adjusting by 7 Degrees Fahrenheit."<<endl;
+		//Notice that the above two outputs should appear on the same line
+		roomTempFaren += 7; //this fancy notation is same as: roomTempFaren = roomTempFaren + 7;
+	}
+	cout<<"Room temp now suitable at "<<roomTempFaren<<" degrees Fahrenheit."<<endl;
+	//The above block ran several times, and as long as (roomTempFaren < 72) was true
+	// after the previous block's execution, the test returned true and the block executed again
+	// Only after the test returned false as a result was the loop block skipped over.
+	
+	//This while loop will not be entered into now that roomTempFaren is too high:
+	while (roomTempFaren < 10 ) {
+		cout << "It's freezing in here!"<<endl;
+	}
+	// even though the while loop is like an "if" block, there is no "else" part to a while loop
+	// If you want to enter a loop only if a condition is met, use if and else blocks around
+	// a while block. For example (using our previous bool variable):
+	
+	if ( isDoorOpen ){ //this test is the same as "if (isDoorOpen == true)"
+		cout<<"WARNING. DOOR OPEN. PLEASE TURN OFF AIR CONDITIONING."<<endl;
+		//You might have noticed that at this point our brilliant program has opened
+		//the door, cranked the air conditioning, and now scolded us for having the door open
+	}
+	else{ //This block is entered if isDoorOpen == false
+		//(This will not execute as I wrote this code, since isDoorOpen was set to true)
+	
+		//Here we have our previous while loop block "nested" in our else block
+		while ( roomTempFaren < 72 ){
+			cout<<"Room temp too low at "<<roomTempFaren;
+			cout<<". Adjusting by 7 Degrees Fahrenheit."<<endl;
+			roomTempFaren += 7; 
+		}
+	
+	}
 	
 	//Do-While
+	/*
+		Where the while loop is useful when you're not sure if the condition is even going
+		to be true once, sometimes you know you want your loop's block to execute at least
+		once, and then to keep executing until a certain condition is met. For example,
+		getting user input and then testing it, and re-asking if the input was bad, is
+		a very common use for a do-while loop.
+		The essence of a do while loop is:
+		
+		do{
+			//code here
+		}while( testIsTrue );
+		
+		The do-while loop is the upside-down while loop, in a way.
+		It is a "post-test" loop, meaning it runs its test after 
+		it runs the code for the first time.
+	*/
+	//Testing user input with Do-While:
 	
+	// A string holds a sequence of characters. It can hold words or sentences.
+	string userInput; //here's a string to hold our user input. 
+	const string correctPass = "password"; //here is a const string that won't change
+	
+	do{ //do this stuff at least once:
+	
+		cout <<"Enter \"password\" to continue: "; //Here we use \" to put a quote symbol in a string
+		cin >> userInput; //get input from console and store it in variable userInput
+		
+	//Here we use != to test if user input is "not equal" to correct password
+	}while( userInput != correctPass ); 
+		
+	//This code will only execute when userInput == correctPass
+	cout<<"You entered \"password\" correctly! Great job!"<<endl;
+	
+	
+	//WARNING: A BOSS FIGHT APPROACHES AHEAD:
+	/*
+		I'll try to throw in a decent intro to the for loop in the 15 lines I have left.
+		Basically: use the for loop when you know exactly how many times you want to loop.
+		A naked for-ever loop block looks like this: for( ; ; ){ }
+		Around those semicolons in the parenthesis go the 3 parts of the "for loop header"
+		Part 1: The Initialization. This happens once. Usually it's "int i = 0"
+		 Where "i" will be a counter used to keep track of times through the loop
+		Part 2: The Test. Just like the test in a while loop. Often it's " i < size"
+		 Where "size" is how many pieces of data are being looked at by the loop.
+		Part 3: The Incrementation. This is most often where your counter is added to.
+		 Most often times this is just "i++" which means set i to its current value + 1.
+		The multiple parts to the for loop header make it look confusing, 
+		but each part is something you've already done before:
+		int i = 0		defining and initializing a variable
+		i < size		testing a conditional statement
+		i = i+1			setting a variable to a value (based on its current value)
+	*/
 	//For Loop
+	int size = 5; // the size of our imaginary data array
+	for(int i = 0  ;  i < size  ;  i++ ){ // Go once for when i = 0, 1, 2, 3 and finally 4
+		cout<<"Blast-off in "<< size-i <<"..."<<endl; //While i counts up, size-i will count down
+	}
+	cout<<"Ignition. We have blast off!"<<endl; //When code gets here, loop has finished
 	
 	
-	//Return 0 to show that no problems occurred during our program
+	//Finally, return 0 to show that no problems occurred during our program
 	return 0;
-}
+}	// And don't forget to close int main() with this final curly brace
+//		THE END 		//
