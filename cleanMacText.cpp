a55ea159e95faf5979ba78e26aebb724d5888ee5
Jay <blaqsparrow@gmail.com>
2018-05-16 20:48:46 -0700

    Make clean copy of text file w/o invisible chars
    Leaves spaces and newlines

--- cleanMacText.cpp
+++ cleanMacText.cpp
@@ -1,28 +1,32 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <cctype>
 using namespace std;
 
 int main(){
 	
 	
 	string fname;
-	cout<<"File Name:";
+	cout<<"This program goes through a .cpp or .txt file and makes a copy\n"
+		"with all invisible characters removed except for spaces and newlines."
+		<<endl;
+	cout<<"Enter file to convert:";
 	getline(cin, fname);
 	
 	ifstream inFile(fname.c_str() );
 	
-	string fOutName = fname+"_pc";
+	string fOutName = "_pc_"+fname;
 	ofstream outFile(fOutName.c_str() );
 	
 	
+	char c;
 	while( inFile ){
-		char c;
-		inFile>>c;
-		if ( isprint(c) || isspace(c) ){
+		c = inFile.get();
+		if ( isprint(c) || isspace(c) || c=='\r'){
 			outFile<<c;
 		}
 	}
-	
+	cout<<"Cleaned-up copy has been made!\n\nCopy name: "<<fOutName<<endl;
 	return 0;
 }
